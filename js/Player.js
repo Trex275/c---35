a27e3039a580bd53d163541a0a0be6c6508fd894
Trex275 <73885729+Trex275@users.noreply.github.com>
2021-01-06 16:55:53 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,28 @@
+class Player{
+    constructor(){
+        this.index = null;
+        this.mame = null;
+        this.distance = 0;
+    }
+    getplayerCount(){
+        database.ref('playercount')
+        .on("value", (data)=>{
+            playercount = data.val();
+        });
+    }
+    updateplayercount(count){
+        database.ref('/')
+        .update({
+            playercount:count
+        })
+        
+    }
+    update(){
+        var playerreference = "players/player" + this.index;
+        database.ref(playerreference)
+        .set({
+            name:this.name,
+            distance:this.distance
+        })        
+    }
+}
